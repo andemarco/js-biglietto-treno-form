bbe792b42b1eecc983c855365a86f7cafda0d782
Andrea De Marco <andreademarco91@gmail.com>
2019-12-19 16:21:19 +0100

    Inizio JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // Chiedere numero km
-var nkm = parseInt(prompt('Quanti km vuoi percorrere?'));
-document.getElementById('nkm').innerHTML = (nkm + 'km');
+var inputKm = document.getElementById('km');
+var kmDaPercorrere = parserInt(inputKm.value);
 
 // Chiedere età
-var eta = parseInt(prompt('Quanti anni hai?'));
-document.getElementById('eta').innerHTML = (eta);
-
+var inputEta = document.getElementById('age');
+var fasciaEta = inputEta.value;
+//Chiedere Nome e Cognome
+var inputName = document.getElementById('name');
+var name = inputName.value
 // Calcolo prezzo totale del viaggio (0.21/KM)
 var costoBiglietto = (0.21 * nkm);
 console.log(costoBiglietto);
 var messaggio = (costoBiglietto.toFixed(2));
 // sconto del 20% per i minorenni
-if (eta < 18 ) {
-  costoBiglietto = (0.21 * nkm) - (0.21*20/100);
-  messaggio = (costoBiglietto.toFixed(2)) + ' (scontato del 20%)';
+if (fasciaEta == 'minorenne' ) {
+  costoBiglietto = costoBiglietto - (0.21*20/100);
 // 40% per gli over 65.
-} else if (eta >= 65) {
-  costoBiglietto = (0.21 * nkm) - (0.21*40/100);
-  messaggio = (costoBiglietto.toFixed(2)) + ' (scontato del 40%)';
+} else if (fasciaEta == 'Over 65') {
+  costoBiglietto = costoBiglietto - (0.21*40/100);
 }
 // STAMPA COSTO BIGLIETTO
 document.getElementById('ticketTreno').innerHTML = ' € ' + messaggio;
